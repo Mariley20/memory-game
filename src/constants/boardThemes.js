e7b0011441a109ca1eacca7dbc32a054edcc5f69
mariley <mariley2018@gmail.com>
2023-03-03 18:24:00 -0500

    feat: add boardThemes constants

---
+++ src/constants/boardThemes.js
@@ -0,0 +1,6 @@
+export const BOARD_THEME_ICONS_ID = 'board_icons'
+export const BOARD_THEME_NUMBERS_ID = 'board_numbers'
+export const BOARD_THEMES = [
+  { id: 'board_icons', text: 'Icons' },
+  { id: 'board_numbers', text: 'Numbers' }
+]
