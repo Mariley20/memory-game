1f408628fb87154a9cb24fb01b75959344c64b7c
mariley <mariley2018@gmail.com>
2023-03-05 10:39:43 -0500

    fix: rename file

---
+++ src/constants/boardSizes.js
@@ -0,0 +1,6 @@
+export const BOARD_SIZE_FOUR_ID = 'board_four'
+export const BOARD_SIZE_SIX_ID = 'board_six'
+export const BOARD_SIZES = [
+  { id: 'board_four', text: '4 x 4', size: 4 },
+  { id: 'board_six', text: '6 x 6', size: 6 }
+]
--- src/constants/boardTypes.js
+++
@@ -1,6 +0,0 @@
-export const BOARD_TYPE_FOUR_ID = 'board_four'
-export const BOARD_TYPE_SIX_ID = 'board_six'
-export const BOARD_TYPES = [
-  { id: 'board_four', text: '4 x 4' },
-  { id: 'board_six', text: '6 x 6' }
-]
