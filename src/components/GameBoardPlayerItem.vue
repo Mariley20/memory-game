32505ff8f8bd58c8a1285b179bf61792d98794ba
mariley <mariley2018@gmail.com>
2023-03-05 10:42:21 -0500

    feat: add GameBoardPlayerItem component

---
+++ src/components/GameBoardPlayerItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="player-item"
+    :class="{'player-item--is-playing': isPlaying}"
+  >
+    player {{ index }}
+    <div>move: {{ moves }}</div>
+    <div>points: {{ points }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    player: { type: Object, default: null },
+    index: { type: Number, default: 0 }
+  },
+  computed: {
+    moves () {
+      return this.player.moves
+    },
+    points () {
+      return this.player.points
+    },
+    isPlaying () {
+      return this.player.isPlaying
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.player-item {
+  &--is-playing {
+    background-color: pink ;
+  }
+}
+</style>
