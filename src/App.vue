4ae8971b39d7d0c6172d513b40d60888f00be05f
mariley <mariley2018@gmail.com>
2023-06-25 11:43:42 -0500

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,16 @@
 <template>
   <div id="app">
-    <div
-      v-if="screenCurrent === SCREEN_START"
-      class="settings-card"
-    >
-      <div>Select Theme</div>
-      <div class="settings-card__themes">
-        <button
-          v-for="(boardTheme, i) in boardThemes"
-          :key="i"
-          @click="selectBoardTheme(boardTheme)"
-        >
-          {{ boardTheme.text }}
-        </button>
-      </div>
-      <div>Numbers Players</div>
-      <div class="settings-card__themes">
-        <button
-          v-for="numberPlayer in numberPlayersAllowed"
-          :key="numberPlayer"
-          @click="selectNumberPlayer(numberPlayer)"
-        >
-          {{ numberPlayer }}
-        </button>
-      </div>
-      <div>Grid Size</div>
-      <div class="settings-card__themes">
-        <button
-          v-for="(boarType, i) in boardSizes"
-          :key="i"
-          @click="selectBoarType(boarType)"
-        >
-          {{ boarType.text }}
-        </button>
-      </div>
-
-      <button @click="startGame">
-        Start Game
-      </button>
-    </div>
-    <GameBoardCard
-      v-if="screenCurrent === SCREEN_BOARD"
-      :game-settings="gameSettings"
-      :players="players"
-    />
+    <nav>
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
-
-<script>
-import { BOARD_THEMES, BOARD_THEME_NUMBERS_ID } from './constants/boardThemes'
-import { BOARD_SIZES, BOARD_SIZE_FOUR_ID } from './constants/boardSizes'
-import GameBoardCard from './components/GameBoardCard.vue'
-
-const SCREEN_START = 'SCREEN_START'
-const SCREEN_BOARD = 'SCREEN_BOARD'
-// const SCREEN_GAME_OVER = 'SCREEN_GAME_OVER'
-
-export default {
-  name: 'App',
-  components: { GameBoardCard },
-  data () {
-    return {
-      players: [],
-      boardThemes: BOARD_THEMES,
-      boardSizes: BOARD_SIZES,
-      numberPlayersAllowed: 4,
-      gameSettings: {
-        boardThemeId: BOARD_THEME_NUMBERS_ID,
-        numbersPlayers: 1,
-        boarSizeId: BOARD_SIZE_FOUR_ID,
-        startTime: '',
-        endTime: '',
-        finished: false
-      },
-      SCREEN_BOARD,
-      SCREEN_START,
-      screenCurrent: SCREEN_START
-    }
-  },
-  computed: {
-    name () {
-      return this.data
-    }
-  },
-  methods: {
-    selectBoardTheme (boardTheme) {
-      console.log('boardTheme', boardTheme)
-      this.gameSettings.boardThemeId = boardTheme.id
-    },
-    selectNumberPlayer (numberPlayer) {
-      this.gameSettings.numbersPlayers = numberPlayer
-    },
-    selectBoarType (boarSize) {
-      console.log('boarSize', boarSize)
-      this.gameSettings.boarSizeId = boarSize.id
-    },
-    startGame () {
-      this.screenCurrent = SCREEN_BOARD
-      this.gameSettings.startTime = new Date()
-    }
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -110,11 +19,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.settings-card {
-  padding: 20px;
+nav {
+  padding: 30px;
 
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div
+      v-if="screenCurrent === SCREEN_START"
+      class="settings-card"
+    >
+      <div>Select Theme</div>
+      <div class="settings-card__themes">
+        <button
+          v-for="(boardTheme, i) in boardThemes"
+          :key="i"
+          @click="selectBoardTheme(boardTheme)"
+        >
+          {{ boardTheme.text }}
+        </button>
+      </div>
+      <div>Numbers Players</div>
+      <div class="settings-card__themes">
+        <button
+          v-for="numberPlayer in numberPlayersAllowed"
+          :key="numberPlayer"
+          @click="selectNumberPlayer(numberPlayer)"
+        >
+          {{ numberPlayer }}
+        </button>
+      </div>
+      <div>Grid Size</div>
+      <div class="settings-card__themes">
+        <button
+          v-for="(boarType, i) in boardSizes"
+          :key="i"
+          @click="selectBoarType(boarType)"
+        >
+          {{ boarType.text }}
+        </button>
+      </div>
+
+      <button @click="startGame">
+        Start Game
+      </button>
+    </div>
+    <GameBoardCard
+      v-if="screenCurrent === SCREEN_BOARD"
+      :game-settings="gameSettings"
+      :players="players"
+    />
+  </div>
+</template>
+
+<script>
+import { BOARD_THEMES, BOARD_THEME_NUMBERS_ID } from '@/constants/boardThemes'
+import { BOARD_SIZES, BOARD_SIZE_FOUR_ID } from '@/constants/boardSizes'
+import GameBoardCard from '@/components/GameBoardCard.vue'
+
+const SCREEN_START = 'SCREEN_START'
+const SCREEN_BOARD = 'SCREEN_BOARD'
+// const SCREEN_GAME_OVER = 'SCREEN_GAME_OVER'
+
+export default {
+  name: 'HomeView',
+  components: { GameBoardCard },
+  data () {
+    return {
+      players: [],
+      boardThemes: BOARD_THEMES,
+      boardSizes: BOARD_SIZES,
+      numberPlayersAllowed: 4,
+      gameSettings: {
+        boardThemeId: BOARD_THEME_NUMBERS_ID,
+        numbersPlayers: 1,
+        boarSizeId: BOARD_SIZE_FOUR_ID,
+        startTime: '',
+        endTime: '',
+        finished: false
+      },
+      SCREEN_BOARD,
+      SCREEN_START,
+      screenCurrent: SCREEN_START
+    }
+  },
+  computed: {
+    name () {
+      return this.data
+    }
+  },
+  methods: {
+    selectBoardTheme (boardTheme) {
+      console.log('boardTheme', boardTheme)
+      this.gameSettings.boardThemeId = boardTheme.id
+    },
+    selectNumberPlayer (numberPlayer) {
+      this.gameSettings.numbersPlayers = numberPlayer
+    },
+    selectBoarType (boarSize) {
+      console.log('boarSize', boarSize)
+      this.gameSettings.boarSizeId = boarSize.id
+    },
+    startGame () {
+      this.screenCurrent = SCREEN_BOARD
+      this.gameSettings.startTime = new Date()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.settings-card {
+  padding: 20px;
+
+}
+</style>
