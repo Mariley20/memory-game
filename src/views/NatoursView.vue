080a18448b38388bb7bbb285c9887b0ee79f4f0d
mariley <mariley2018@gmail.com>
2023-06-25 19:26:54 -0500

    add Natours page and its header

---
+++ src/views/NatoursView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NatoursHeader />
+  </div>
+</template>
+
+<script>
+import NatoursHeader from '@/components/NatoursHeader.vue'
+export default {
+  components: {
+    NatoursHeader
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
