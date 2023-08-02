df05f7b1c1b9395b1f4c18ae7dc26353bf3c4f1e
mariley <mariley2018@gmail.com>
2023-08-02 17:43:20 -0500

    update depoy file

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Mariley20/memory-game.git main:gh-pages
-
+# ❌ git push -f git@github.com:Mariley20/memory-game.git main:gh-pages
+git push -f https://github.com/Mariley20/memory-game.git main:gh-pages
 cd -
