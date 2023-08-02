aa45dc995f0bda678d14fdf07556bd281028d01b
mariley <mariley2018@gmail.com>
2023-08-02 17:02:55 -0500

    feat: add header button

--- src/components/NatoursHeader.vue
+++ src/components/NatoursHeader.vue
@@ -12,6 +12,10 @@
         <span class="heading-primary-main">Outdoors</span>
         <span class="heading-primary-sub">is where life happens</span>
       </h1>
+      <a
+        href="#"
+        class="btn btn-white btn-animated"
+      >Discover our tours</a>
     </div>
   </header>
 </template>
@@ -47,15 +51,16 @@ header {
 .heading-primary {
   color: #fff;
   text-transform: uppercase;
-
+  backface-visibility: hidden;
+  margin-bottom: 60px;
 }
 
 .text-box {
   position: absolute;
   top: 40%;
   left: 50%;
-  background-color: red;
   transform: translate(-50%, -50%);
+  text-align: center;
 }
 
 .heading-primary-main {
@@ -63,6 +68,8 @@ header {
   font-size: 60px;
   font-weight: 400;
   letter-spacing: 35px;
+
+  animation: moveInLeft 1s ease-out;
 }
 
 .heading-primary-sub {
@@ -70,6 +77,102 @@ header {
   font-size: 20px;
   font-weight: 700;
   letter-spacing: 17.4px;
-
+  animation-name: moveInRight;
+  animation-duration: 1s;
+  animation-timing-function: ease-out;
 }
+
+@keyframes moveInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+
+  80% {
+    transform: translateX(10px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+@keyframes moveInRight {
+  0% {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+
+  80% {
+    transform: translateX(-10px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+
+@keyframes moveInBottom {
+  0% {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translate(0);
+  }
+}
+
+.btn:link,
+.btn:visited {
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: 15px 40px;
+  display: inline-block;
+  border-radius: 100px;
+  transition: all .2s;
+  position: relative;
+}
+
+.btn:hover {
+  transform: translateY(-3px);
+  box-shadow: 0 10px 20px rgb(0, 0, 0,.2);
+}
+.btn:active {
+  transform: translateY(-1px);
+  box-shadow: 0 5px 10px rgb(0, 0, 0,.2);
+}
+
+.btn-white {
+  background-color: white;
+  color: #777;
+}
+
+.btn::after {
+  content: '';
+  display: inline-block;
+  height: 100%;
+  width: 100%;
+  border-radius: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  transition: all .4s;
+}
+.btn-white::after {
+  background-color: white;
+}
+.btn:hover::after {
+  transform: scaleX(1.4) scaleY(1.6);
+  opacity: 0;
+}
+
+.btn-animated {
+  animation: moveInBottom .5s ease-out .75s;
+  animation-fill-mode: backwards;
+}
+
 </style>
