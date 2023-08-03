73cc38ffcd8203c09fb932e4adf1346dc07b9154
mariley <mariley2018@gmail.com>
2023-08-02 20:55:05 -0500

    feat: test scss structure

--- src/components/NatoursHeader.vue
+++ src/components/NatoursHeader.vue
@@ -1,20 +1,20 @@
 <template>
   <header class="header">
-    <div class="logo-box">
+    <div class="header__logo-box">
       <img
         :src="require('@/assets/img/logo-white.png')"
         alt="Logo"
-        class="logo"
+        class="header__logo"
       >
     </div>
-    <div class="text-box">
+    <div class="header__text-box">
       <h1 class="heading-primary">
-        <span class="heading-primary-main">Outdoors</span>
-        <span class="heading-primary-sub">is where life happens</span>
+        <span class="heading-primary--main">Outdoors</span>
+        <span class="heading-primary--sub">is where life happens</span>
       </h1>
       <a
         href="#"
-        class="btn btn-white btn-animated"
+        class="btn btn--white btn--animated"
       >Discover our tours</a>
     </div>
   </header>
@@ -27,59 +27,58 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-header {
+.header {
   height: 95vh;
-  background-image: linear-gradient(to right bottom, rgba(126, 213, 111, 0.8), rgba(40, 180, 133, 0.8)), url(@/assets/img/hero.jpg);
+  background-image: linear-gradient(to right bottom, rgba($color-primary-light, 0.8), rgba($color-primary-dark, 0.8)), url(@/assets/img/hero.jpg);
   background-size: cover;
   background-position: top;
   position: relative;
 
   clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
-}
 
-.logo-box {
-  position: absolute;
-  top: 40px;
-  left: 40px;
+  &__logo-box {
+    position: absolute;
+    top: 40px;
+    left: 40px;
+  }
 
-}
+  &__logo {
+    height: 35px;
+  }
 
-.logo {
-  height: 35px;
+  &__text-box {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
 }
 
 .heading-primary {
-  color: #fff;
+  color: $color-white;
   text-transform: uppercase;
   backface-visibility: hidden;
   margin-bottom: 60px;
-}
 
-.text-box {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
+  &--main {
+    display: block;
+    font-size: 60px;
+    font-weight: 400;
+    letter-spacing: 35px;
 
-.heading-primary-main {
-  display: block;
-  font-size: 60px;
-  font-weight: 400;
-  letter-spacing: 35px;
+    animation: moveInLeft 1s ease-out;
+  }
 
-  animation: moveInLeft 1s ease-out;
-}
-
-.heading-primary-sub {
-  display: block;
-  font-size: 20px;
-  font-weight: 700;
-  letter-spacing: 17.4px;
-  animation-name: moveInRight;
-  animation-duration: 1s;
-  animation-timing-function: ease-out;
+  &--sub {
+    display: block;
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 17.4px;
+    animation-name: moveInRight;
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+  }
 }
 
 @keyframes moveInLeft {
@@ -97,6 +96,7 @@ header {
     transform: translate(0);
   }
 }
+
 @keyframes moveInRight {
   0% {
     opacity: 0;
@@ -125,54 +125,59 @@ header {
   }
 }
 
-.btn:link,
-.btn:visited {
-  text-transform: uppercase;
-  text-decoration: none;
-  padding: 15px 40px;
-  display: inline-block;
-  border-radius: 100px;
-  transition: all .2s;
-  position: relative;
-}
+.btn {
 
-.btn:hover {
-  transform: translateY(-3px);
-  box-shadow: 0 10px 20px rgb(0, 0, 0,.2);
-}
-.btn:active {
-  transform: translateY(-1px);
-  box-shadow: 0 5px 10px rgb(0, 0, 0,.2);
-}
+  &:link,
+  &:visited {
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 15px 40px;
+    display: inline-block;
+    border-radius: 100px;
+    transition: all .2s;
+    position: relative;
+  }
 
-.btn-white {
-  background-color: white;
-  color: #777;
-}
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgb($color-black, .2);
 
-.btn::after {
-  content: '';
-  display: inline-block;
-  height: 100%;
-  width: 100%;
-  border-radius: 100px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  transition: all .4s;
-}
-.btn-white::after {
-  background-color: white;
-}
-.btn:hover::after {
-  transform: scaleX(1.4) scaleY(1.6);
-  opacity: 0;
-}
+    &::after {
+      transform: scaleX(1.4) scaleY(1.6);
+      opacity: 0;
+    }
+  }
 
-.btn-animated {
-  animation: moveInBottom .5s ease-out .75s;
-  animation-fill-mode: backwards;
-}
+  &:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgb($color-black, .2);
+  }
 
+  &--white {
+    background-color: $color-white;
+    color: $color-grey-dark;
+
+    &::after {
+      background-color: $color-white;
+    }
+  }
+
+  &::after {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+    border-radius: 100px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transition: all .4s;
+  }
+
+  &--animated {
+    animation: moveInBottom .5s ease-out .75s;
+    animation-fill-mode: backwards;
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/memory-game/'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "@/styles/abstracts/_variables.scss";'
+      }
+    }
+  }
 })
